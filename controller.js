6cd9c32e189d75f023efa56b2703c06692cd9f18
Mike <mcarey26@gmail.com>
2018-03-19 16:30:50 -0600

    initial application printing function based on demo base;

--- controller.js
+++ controller.js
@@ -68,25 +68,6 @@ Vue.component('claim', {
 			this.model.children.push(child);
 			this.$forceUpdate();
 		},
-		select: function(){
-			//console.log('select claim');
-			this.$eventHub.$emit('unselect-claim');
-			var claim = this;
-			claim_update = true;
-			Vue.nextTick(function(){
-				if (claim_update){
-					claim.model.selected = true;
-					claim.$forceUpdate();
-					claim_update = false;
-				}
-				
-			})
-		},
-		unselect: function(){
-			//console.log('unselect claim')
-			this.model.selected = false;
-			this.$forceUpdate();
-		},
 	},
 	created: function(){
 		this.model.children = this.model.children || [];
@@ -113,27 +94,13 @@ Vue.component('feature',{
 			var new_text = this.$el.innerText;
 			this.$el.innerText = new_text;
 	  		this.model.text = new_text;
-		},
-		select: function(){
-			//console.log('select feature');
-			this.$eventHub.$emit('unselect-claim');
-			var feature = this;
-			Vue.nextTick(function(){
-				feature.$emit('select');
-				feature.model.selected = true;
-			})
-		},
-		unselect: function(){
-			//console.log('unselect feature')				
-			this.model.selected = false;
+	  		this.model.components = [featureComponent(new_text)];
 		},
 	},
 	created: function(){
 		this.model.text = this.model.text || '';
-		this.model.components = this.model.components || ['component'];
+		this.model.components = this.model.components || ['application component'];
 		this.model.selected = false;
-		//event handlers
-		this.$eventHub.$on('unselect-claim', this.unselect);
 	},
 })
 
@@ -180,10 +147,6 @@ Vue.component('claims', {
 				child.deleteChild(grandchild_index);
 			}
 		},
-		flowchart: function(features){
-			console.log(features);
-			this.$eventHub.$emit('flowchart', features);
-		}
 	},
 })
 
@@ -208,16 +171,15 @@ var application = new Vue({
 	el: '#application',
 	data: function() {
 		return {
-			app: {info: {}, claims: {}},
+			app: {info: {}, claims: []},
 		}
 	},
 	methods: {
 		print: function(){
 			console.log('get specification');
-			getFile(this.app, 'spec');
-
-			console.log('get drawings');
-			getFile(this.app, 'draw');
+			var demo_app = processApp(this.app);
+			console.log(demo_app);
+			//getApp(demo_app);
 		},
 	},
 	created: function() {
@@ -252,11 +214,19 @@ function importData(doc, sheet, callback){
 	});
 }
 
-function getFile(data, type){
+function getApp(data){
+	getFile(data, 'spec', function(){
+		getFile(data, 'draw');
+	})
+}
+
+function getFile(data, type, callback){
 	string_data = JSON.stringify(data).replace(/&/g, 'and')
 	request("POST", server_url + type, string_data, function(file_name){
 		var download_url = server_url + file_name;
 		downloadURL(download_url, file_name);
+		if (callback)
+			callback();
 	});
 }
 
@@ -286,3 +256,174 @@ function clone(object){
 	return Object.assign({}, object);
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////// DEMO //////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+var vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
+var articles = ['a', 'the', 'an', 'each','one', 'at', 'another', 'said'];
+
+function gerund(verb) {
+	var last = verb.charAt(verb.length - 1).toLowerCase();
+	var second_last = verb.charAt(verb.length - 2).toLowerCase();
+	var third_last = verb.charAt(verb.length - 3).toLowerCase();
+
+	if (verb.slice(-3).toLowerCase() == 'ing' && verb.length > 4){
+	}
+	else if (second_last == 'i' && last == 'e') {
+		verb = verb.slice(0, -2) + 'ying';
+	}
+	else if (last == 'e') {
+		verb = verb.slice(0, -1) + 'ing';
+	}
+	else if (vowels.indexOf(third_last) < 0 && vowels.indexOf(last) < 0 && vowels.indexOf(second_last) > -1 && last != 'w' && last != 'x' && last != 'y'){
+		verb += last + 'ing';
+	}
+	else {
+		verb += 'ing';
+	}
+
+	return verb;
+}
+
+function featureComponent(text){
+	text = text || '';
+	words = text.trim().split(' ');
+
+	if (articles.indexOf(words[0]) > -1)
+		return ('application component');
+	else
+		return gerund(words[0]) + ' component';
+
+}
+
+function processApp(app){
+	var demo_app = demo_app_base;
+	Object.keys(app.info).forEach(key => {
+		if (app.info.key)
+			demo_app.info.key = app.info.key
+	})
+
+	demo_app.claims = app.claims;
+
+	return demo_app;
+}
+
+var demo_app_base = {
+	"info": {
+		"client": "Demo",
+		"identifier": "DEMO_001",
+		"title": "{title of the invention}",
+		"field": "{field of the invention}",
+		"description": "{description of the invention}",
+		"inventor": "{inventor}",
+	},
+	"parts": [{
+		"sentences": ["A processor may include an intelligent hardware device, (e.g., a general-purpose processing component, a DSP, a CPU, a microcontroller, an ASIC, an FPGA, a programmable logic device, a discrete gate or transistor logic component, a discrete hardware component, or any combination thereof). In some cases, the processor may be configured to operate a memory array using a memory controller. In other cases, a memory controller may be integrated into processor. The processor may be configured to execute computer-readable instructions stored in a memory to perform various functions. In some examples, a processor may include special purpose components for modem processing, baseband processing, digital signal processing, or transmission processing. In some examples, the processor may comprise a system-on-a-chip."],
+		"selected": true,
+		"topic": "processor",
+		"type": "detail",
+		"figure": "1"
+	}, {
+		"sentences": ["Memory may include RAM, ROM, or a hard disk. The memory may be solid state or a hard disk drive, and may include store computer-readable, computer-executable software including instructions that, when executed, cause a processor to perform various functions described herein. In some cases, the memory may contain, among other things, a BIOS which may control basic hardware or software operation such as the interaction with peripheral components or devices. In some cases, a memory controller may operate memory cells as described herein. In some cases, memory controller may include a row decoder, column decoder, or both. In some cases, memory cells within a memory may store information in the form of a logical state."],
+		"selected": true,
+		"topic": "memory",
+		"type": "detail",
+		"figure": "1"
+	}],
+	"claims": [{
+		"features": [{
+			"text": "do a first thing",
+			"components": ["component"],
+			"selected": false
+		}, {
+			"text": "do a second thing",
+			"components": ["component"],
+			"selected": true
+		}],
+		"children": [],
+		"variations": ["method"],
+		"category": "process",
+		"selected": true
+	}],
+	"figures": [{
+		"category": "diagram",
+		"description": "a block diagram of a computing device",
+		"text": [],
+		"components": [{
+			"name": "computing device",
+			"children": [{
+				"name": "processor",
+				"children": []
+			}, {
+				"name": "application component",
+				"children": []
+			}, {
+				"name": "memory",
+				"children": []
+			}]
+		}],
+		"steps": [],
+		"selected": false
+	}, {
+		"category": "flowchart",
+		"description": "a process for {DESCRIPTION}",
+		"text": [],
+		"components": [],
+		"steps": [],
+		"selected": false
+	}],
+	"acronyms": [{
+		"abbreviation": "RAM",
+		"definition": "random access memory"
+	}, {
+		"abbreviation": "ROM",
+		"definition": "read only memory"
+	}, {
+		"abbreviation": "ASIC",
+		"definition": "application specific integrated circuit"
+	}, {
+		"abbreviation": "FPGA",
+		"definition": "field programmable gate array"
+	}],
+	"configuration": {
+		"numbering": "true",
+		"indent": "0",
+		"font": "Times New Roman",
+		"header": " ",
+		"footer": "Attorney Docket No. DEMO_001",
+		"template": "1YX9yCCXqeqlbgvO6vL2Of_EO0HJGsM4a",
+		"transition": "in some cases",
+		"conclusion": "in accordance with aspects of the present disclosure",
+		"flowchart": "In some examples, these operations may be performed by a processor executing a set of codes to control functional elements of an apparatus. Additionally or alternatively, the processes may be performed using special-purpose hardware. Generally, these operations may be performed according to the methods and processes described in accordance with aspects of the present disclosure. For example, the operations may be composed of various substeps, or may be performed in conjunction with other operations described herein.",
+		"process": [{
+			"category": "process",
+			"type": "method",
+			"verb": "gerund"
+		}, {
+			"category": "process",
+			"type": "apparatus",
+			"verb": "infinitive",
+			"prefix": "",
+			"preamble": "An apparatus for {DESCRIPTION}, comprising: a processor and a memory storing instructions and in electronic communication with the processor, the processor being configured to execute the instructions to:",
+			"transition": "the processor being further configured to execute the instructions to",
+			"summary": "An apparatus for {DESCRIPTION} is described. The apparatus may include a processor, memory in electronic communication with the processor, and instructions stored in the memory. The instructions may be operable to cause the processor to"
+		}, {
+			"category": "process",
+			"type": "non-transitory computer-readable medium",
+			"verb": "infinitive",
+			"prefix": "",
+			"preamble": "A non-transitory computer readable medium storing code for {DESCRIPTION}, the code comprising instructions executable by a processor to:",
+			"transition": "the code further comprising instructions executable by the processor to",
+			"summary": "A non-transitory computer readable medium storing code for {DESCRIPTION} is described. In some examples, the code comprises instructions executable by a processor to:"
+		}, {
+			"category": "process",
+			"type": "apparatus",
+			"verb": "gerund",
+			"prefix": "means for"
+		}],
+		"device": [],
+		"drawings": "1jZfm-gyWC1XbyiREGhSvZmG_v8f7JHDH",
+		"summary": "long",
+	}
+}
